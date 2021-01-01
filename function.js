10265c3bb396f50674ed39b6fb13feabe27bae02
NFL07AHMAD <nownaufal07@gmail.com>
2021-01-01 21:52:47 +0700

    besar

---
+++ function.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    (function(){
+        let d = new Date();
+        let day = d.getDay();
+        if(day == 0 || day == 6){
+            $('table').hide();
+            $('.card').hide();
+            $('#libur').show();
+        }
+        if (day == 1){
+            $('.card').show();
+            $('table').show();
+            $('#mapel-1').text('B.Jawa');
+            $('#mapel-2').text('PJOK');
+            $('#mapel-3').text('Bk');
+        }
+        if (day == 2){
+            $('.card').show();
+            $('table').show();
+            $('#mapel-1').text('PPKN');
+            $('#mapel-2').text('IPA');
+            $('#mapel-3').text('TIK');
+        }
+        if (day == 3){
+            $('.card').show();
+            $('table').show();
+            $('#mapel-1').text('IPS');
+            $('#mapel-2').text('B.Inggris');
+            $('#mapel-3').text('Seni');
+        }
+        if (day == 4){
+            $('.card').show();
+            $('table').show();
+            $('#mapel-1').text('Prakarya');
+            $('#mapel-2').text('B.Indo');
+            $('#mapel-3').text('Agama');
+        }
+        if (day == 5){
+            $('.card').show();
+            $('table').show();
+            $('#mapel-1').text('Matematika');
+            $('#mapel-2').text('Pramuka');
+            $('#mapel-3').text('');
+            $('tr:last>td').hide()
+        }
+        if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5){
+            $('#libur').hide()
+        }
+        else if(isNaN(day)){
+            $('html').empty().html('<h1 style="text-align:center; font-size:380%; color: red; background: black;">ERROR :(</h1>');
+        }
+    }());
+    (function(){
+        $('#submit').click(function(){
+            window.location.href='#table'
+        })
+    }())
+});
