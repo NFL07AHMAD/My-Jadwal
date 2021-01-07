3be7fffb29dac7ef7af3c1e1936425c5a76e62ec
NFL07AHMAD <nownaufal07@gmail.com>
2021-01-07 10:27:52 +0700

    o

--- js/function.js
+++ js/function.js
@@ -1,41 +1,41 @@
-$(document).ready(function(){
-    (function(){
+$(document).ready(function () {
+    (function () {
         let d = new Date();
         let day = d.getDay();
-        if(day == 0 || day == 6){
+        if (day == 0 || day == 6) {
             $('table').hide();
             $('.card').hide();
             $('#libur').show();
         }
-        if (day == 1){
+        if (day == 1) {
             $('.card').show();
             $('table').show();
             $('#mapel-1').text('B.Jawa');
             $('#mapel-2').text('PJOK');
             $('#mapel-3').text('Bk');
         }
-        if (day == 2){
+        if (day == 2) {
             $('.card').show();
             $('table').show();
             $('#mapel-1').text('PPKN');
-            $('#mapel-2').text('IPA');
+            $('#mapel-2').text('Agama');
             $('#mapel-3').text('TIK');
         }
-        if (day == 3){
+        if (day == 3) {
             $('.card').show();
             $('table').show();
             $('#mapel-1').text('IPS');
             $('#mapel-2').text('B.Inggris');
             $('#mapel-3').text('Seni');
         }
-        if (day == 4){
+        if (day == 4) {
             $('.card').show();
             $('table').show();
-            $('#mapel-1').text('Prakarya');
+            $('#mapel-1').text('IPA');
             $('#mapel-2').text('B.Indo');
-            $('#mapel-3').text('Agama');
+            $('#mapel-3').text('Prakarya');
         }
-        if (day == 5){
+        if (day == 5) {
             $('.card').show();
             $('table').show();
             $('#mapel-1').text('Matematika');
@@ -43,16 +43,27 @@ $(document).ready(function(){
             $('#mapel-3').text('');
             $('tr:last>td').hide()
         }
-        if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5){
-            $('#libur').hide()
-        }
-        else if(isNaN(day)){
+        if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5) {
+            $('#libur').hide();
+            $('.pass').show();
+            $('.pass_back').show();
+        } else if (isNaN(day)) {
             $('html').empty().html('<h1 style="text-align:center; font-size:380%; color: red; background: black;">ERROR :(</h1>');
         }
     }());
-    (function(){
-        $('#submit').click(function(){
-            window.location.href='#table'
+    (function () {
+        $('#wrong').hide()
+        $('#submit').click(function () {
+            window.location.href = '#table'
         })
+$('#submit').click(function () {
+    let password = $('#input').val(); 
+    if (password == "password") {
+        //some event
+    } else {
+        //some event
+    }
+})
     }())
+
 });
