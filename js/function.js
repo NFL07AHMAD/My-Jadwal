efc08f19dbe286170c964c678f0780d56ca7209d
NFL07AHMAD <nownaufal07@gmail.com>
2021-01-08 16:31:17 +0700

    0

--- js/function.js
+++ js/function.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
             $('#libur').hide();
             $('.pass').show();
             $('.pass_back').show();
-        } else if (isNaN(day)) {
+        } else {
             $('html').empty().html('<h1 style="text-align:center; font-size:380%; color: red; background: black;">ERROR :(</h1>');
         }
     }());
@@ -56,14 +56,20 @@ $(document).ready(function () {
         $('#submit').click(function () {
             window.location.href = '#table'
         })
-$('#submit').click(function () {
-    let password = $('#input').val(); 
-    if (password == "password") {
-        //some event
-    } else {
-        //some event
-    }
-})
+        $('#submit_2').click(function () {
+            let password = $('#password').val();
+            if (password == "NFL07") {
+                $('.pass_back').hide();
+                $('.pass').hide();
+                $('#password').val("")
+            } else {
+                $('#wrong').show();
+            }
+        })
+        $('#link_youtube_kirim').click(function(){
+            let link = $('#link_youtube_insert').val()
+            $('#costum_link_youtube').attr({"src" : link})
+            $('#link_youtube_insert').val("")
+        })
     }())
-
 });
